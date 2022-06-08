3a9ccdaf0e0238a8501bc7def74efb796e6f79f5
Mac L <macattackairman@gmail.com>
2022-06-08 15:28:38 +0100

    chaged API Key and ID to variables

--- Policy/get.js
+++ Policy/get.js
@@ -1,8 +1,8 @@
 let xhr = new XMLHttpRequest();
 xhr.open("POST", "https://api-{fqdn}/public_api/v1/endpoints/get_policy");
 
-xhr.setRequestHeader("x-xdr-auth-id", "API_KEY_ID");
-xhr.setRequestHeader("Authorization", "API_KEY");
+xhr.setRequestHeader("x-xdr-auth-id", (API_KEY_ID));
+xhr.setRequestHeader("Authorization", (API_KEY));
 xhr.setRequestHeader("Content-Type", "application/json");
 
 xhr.onload = () => console.log(xhr.responseText);
--- Policy/get.sh
+++
@@ -1,8 +0,0 @@
-curl -X POST https://api-{fqdn}/public_api/v1/endpoints/get_policy/ \
--H "x-xdr-auth-id:{API_KEY_ID}" \
--H "Authorization:{API_KEY}" \
--H "Content-Type:application/json" \
--d '{
-   "request_data":{
-      "endpoint_id":"<endpoint ID>"
-}' 
